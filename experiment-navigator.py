a662deff6a744a55c5a86597512355a30775e2a1
jnudaniel <jnudaniel@users.noreply.github.com>
2018-03-08 19:08:15 -0800

    added python script file

---
+++ experiment-navigator.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# Experiment Navigator user interaction script
+# Julia Daniel, Akhila Moturu, Eric Cramer, and Gaby Steiner
+# Final project for CS 270 / BMI 210, Winter 2018
+# ----------------------------------------------------------
+
+
+
+if __name__ == "__main__":
