855461e5323a28da9331e212cf9adab6c5d71f68
Akhila Moturu <amoturu@stanford.edu>
2018-03-10 12:05:55 -0800

    added test code for reasoning pizza

--- experiment-navigator.py
+++ experiment-navigator.py
@@ -5,6 +5,19 @@
 # ----------------------------------------------------------
 
 from owlready2 import *
+onto = get_ontology("file://pizza.owl").load()
 
 if __name__ == "__main__":
     print("hello")
+    # acetaminophen   = ActivePrinciple("acetaminophen")
+    my_pizza = onto.Pizza("pizza1")
+    garlic = onto.GarlicTopping()
+    mozz = onto.MozzarellaTopping()
+    redon = onto.RedOnionTopping()
+
+    my_pizza.hasTopping = [garlic, mozz, redon]
+    print(my_pizza.hasTopping)
+
+    sync_reasoner()
+
+    print(my_pizza.__class__)
